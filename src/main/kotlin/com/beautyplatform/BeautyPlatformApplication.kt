acfcf5820d3c4a6a5567c7de756815666c134ca0
heemanglee <wishhopelee@gmail.com>
2026-03-14 16:39:50 +0900

    first commit

---
+++ src/main/kotlin/com/beautyplatform/BeautyPlatformApplication.kt
@@ -0,0 +1,11 @@
+package com.beautyplatform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BeautyPlatformApplication
+
+fun main(args: Array<String>) {
+	runApplication<BeautyPlatformApplication>(*args)
+}
---
+++ src/test/kotlin/com/beautyplatform/BeautyPlatformApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.beautyplatform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BeautyPlatformApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
